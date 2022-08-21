f0021a34eb042902461ce819ce649cca39eec939
Veerrohit Veeravadivel <veerrohitv@macbook-pro.lan>
2022-08-21 13:23:27 -0700

    omg we're screwed

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const uploadImage = require("./uploadImage.js");
 const multer = require("multer");
 var compression = require("compression");
 
-app.use(express.json({ limit: "4mb" }));
+app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(compression());
